2f2889dc5bd68aad20554f1b2a53411280be5ed3
Chelsea <chelsealexander@gmail.com>
2018-04-20 15:50:38 -0700

    Added navbar, footer, text input component, and button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { NgModule, NO_ERRORS_SCHEMA, ViewChild } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
+import { ButtonPrimaryComponent } from './button-primary/button-primary.component';
+import { PageFooterComponent } from './page-footer/page-footer.component';
+import { TextInputComponent } from './text-input/text-input.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AuthService } from './services/auth.service';
 
+const appRoutes: Routes = [
+  {path:'', component: AppComponent},
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonPrimaryComponent,
+    PageFooterComponent,
+    TextInputComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(appRoutes),
     MDBBootstrapModule.forRoot(),
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
---
+++ src/app/text-input/text-input.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-text-input',
+  templateUrl: './text-input.component.html',
+  styleUrls: ['./text-input.component.css']
+})
+export class TextInputComponent implements OnInit {
+
+  @Input('labelText') labelString:string;
+  @Input('name') nameString:string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
