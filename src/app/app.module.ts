dc30403aa1ef36bafc073bce4b5221ac088dd4b7
Lucas Phillips <lpquotec@gmail.com>
2018-04-20 17:24:57 -0700

    Added Login Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, NO_ERRORS_SCHEMA, ViewChild } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import{FormsModule, ReactiveFormsModule}from '@angular/forms';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './register/register.component';
 import { ButtonPrimaryComponent } from './button-primary/button-primary.component';
 import { PageFooterComponent } from './page-footer/page-footer.component';
 import { TextInputComponent } from './text-input/text-input.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AuthService } from './services/auth.service';
+import{ RegisterComponent}from './register/register.component';
+import{LoginComponent}from './login/login.component';
 
 const appRoutes: Routes = [
-  {path:'', component: RegisterComponent},
+  {path:'', component: AppComponent},
+  {path:'login', component: LoginComponent},
   {path:'register', component: RegisterComponent},
-  {path:'**', component: RegisterComponent},
 ];
 
 @NgModule({
@@ -24,14 +25,15 @@ const appRoutes: Routes = [
     PageFooterComponent,
     TextInputComponent,
     NavbarComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes),
     MDBBootstrapModule.forRoot(),
+    FormsModule,
+    ReactiveFormsModule
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   providers: [AuthService],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder) {
+    this.loginForm = this.formbuilder.group({
+      email: [null, [Validators.email, Validators.required]],
+      password: [null, [Validators.required, Validators.minLength(6)]]
+    })
+    this.loginForm.valueChanges.subscribe(() => {
+      // es6 syntax for functions. https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+      console.log(this.loginForm.value );
+    })
+  }
+
+  login(): void {
+
+    console.log(this.loginForm.value, "Login clicked");
+  }
 
   ngOnInit() {
   }
